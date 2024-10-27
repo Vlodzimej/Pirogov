b8f4bd341c65983ddc41fd0aa9c271945fa05b4f
Vladimir Amelkin <vamelkin@vbankcenter.ru>
2024-10-27 16:51:20 +0300

    Added comments

--- Pirogov/Controllers/MainViewController.swift
+++ Pirogov/Controllers/MainViewController.swift
@@ -38,11 +38,13 @@ class MainViewController: UIViewController {
             }
         }
     }
-    
+    /// Имя коллекции в базе данных, к которой подключаемся для получения данных
+    let firestoreProductCollectionName = "products"
+    /// Слушатель для базы данных Firestore
     private var listener: ListenerRegistration?
+    /// Список «документов» в коллекции базы данных, к которой подключились (см. метод baseQuery())
+    /// Это пока не используется, но пусть будет, чтобы не потерять.
     private var documents: [DocumentSnapshot] = []
-    
-    private var products: [Product] = []
     
     // MARK: VC Lifecycle
     override func viewDidLoad() {
@@ -74,29 +76,44 @@ class MainViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        // Перед отображением вьюшки подписываемся на прослушку базы данных
         observeQuery()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        // Если эта вьюшка скрывается, тогда удаляем слушателя, чтобы небыло ненужной работы в фоне
+        stopObserving()
+    }
+    
+    // MARK: Private methods
+    // Метод подписки слушателя (listener) на события базы данных Firestore
     private func observeQuery() {
         guard let query else { return }
         stopObserving()
         
+        // Подписываем слушателя
         listener = query.addSnapshotListener { [weak self] snapshot, error in
+            // snapshot это слепок базы данных в определённый момент времени
+            // этим моментом может быть момент подключения, либо когда у БД происходят какие-то изменения (редактирование/добавление/удаление)
             guard let self, let snapshot else {
                 debugPrint("Error fetching snapshot results: \(error!)")
                 return
             }
             
+            // У слепка есть свойство documents в котором содержатся записи (в нашем случае это продукты)
+            // Начинаем обход каждого документа и пробразуем этот массив докуметов в массив наших моделей
             let models = snapshot.documents.map { (document) -> Product in
                 let model: Product?
                 do {
+                    // Приводим неизвестный тип к ожидаемому. В нашем случае это Product
                     model = try document.data(as: Product.self)
                 } catch {
                     fatalError(
                         "Unable to initialize type \(Product.self) with dictionary \(document.data()): \(error)"
                     )
                 }
-                
+                // Если всё ок, то достаём из документа идентификатор и назначаем полученной модели, так как само содержимое документа в себе его не содержит,
+                // соответственно айдишник неоткуда декодировать. Это не очень прикольно и нужно получше разобратсья с этим вопросом, но пока можно так.
                 if var model, let id = Int(document.documentID) {
                     model.id = id
                     return model
@@ -105,10 +122,11 @@ class MainViewController: UIViewController {
                     fatalError("Missing document of type \(Product.self) at \(document.reference.path)")
                 }
             }
-            self.products = models
             self.documents = snapshot.documents
-            self.menu = GroupProducts.setup(with: models)
             
+            /// Формируем данные для таблицы из полученных от БД моделей
+            self.menu = GroupProducts.setup(with: models)
+            /// Обновляем таблицу
             productsTableView.reloadData()
         }
     }
@@ -118,7 +136,7 @@ class MainViewController: UIViewController {
     }
     
     private func baseQuery() -> Query {
-        return Firestore.firestore().collection("products").limit(to: 50)
+        return Firestore.firestore().collection(firestoreProductCollectionName).limit(to: 50)
     }
     
     //  MARK: - ScrollView
@@ -166,7 +184,6 @@ extension MainViewController {
             
         ])
     }
-    
     
     private func setupCustomTableView() {
         view.addSubview(productsTableView)
